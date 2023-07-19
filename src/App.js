9e56cf40ce0e8ad853c7f98f36c32c90e2a7374c
Lady30-19 <laura.apaza1430@gmail.com>
2023-07-18 19:25:00 -0500

    se agrego tres componentes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import C01componente from './components/C01componente';
 import C02contador from './components/C02contador';
 import C03doblecontador from './components/C03doblecontador';
+import C04variable from './components/C04variable';
+import C05operador from './components/C05operadorTernario';
+import C06matriz from './components/C06matriz1';
 
 function App() {
   return (
@@ -13,6 +16,13 @@ function App() {
       <C02contador />
       <h3>Tercer componente</h3>
       <C03doblecontador />
+      <h3>Cuarto componente</h3>
+      <C04variable xVariable="Hola mundo..." />
+      <h3>Quinto componente</h3>
+      <C05operador />
+      <h3>Sexto componente</h3>
+      <C06matriz />
+
     </div>
   );
 }
